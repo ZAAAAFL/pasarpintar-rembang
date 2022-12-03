b5695341b36d08d8e80067cb9279df3507f9b201
Ardiputra614 <ardianto8306@gmail.com>
2022-12-03 09:48:02 +0700

    Cart

--- resources/js/Pages/Cart.js
+++ resources/js/Pages/Cart.js
@@ -25,9 +25,9 @@ export default function Cart(props) {
                 </div>
                 <div className="flex mt-10 mb-5">
                   <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
-                  <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Quantity</h3>
-                  <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Price</h3>
-                  <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
+                  <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">Quantity</h3>
+                  <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">Price</h3>
+                  <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">Total</h3>
                 </div>
                 <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
                   <div className="flex w-2/5">
