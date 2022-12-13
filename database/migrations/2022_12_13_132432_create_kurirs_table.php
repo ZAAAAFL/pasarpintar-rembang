143c737011f6ed6e0d4a21a8ae702cbfc9ecf4c1
Ardiputra614 <ardianto8306@gmail.com>
2022-12-13 13:52:58 +0700

    kurir

--- database/migrations/2022_12_13_132432_create_kurirs_table.php
+++ database/migrations/2022_12_13_132432_create_kurirs_table.php
@@ -14,6 +14,7 @@ class CreateKurirsTable extends Migration
   public function up()
   {
     Schema::create('kurirs', function (Blueprint $table) {
+      $table->id();
       $table->foreignId('idUser');
       $table->foreignId('idToko')->nullable();
       $table->string('name');
