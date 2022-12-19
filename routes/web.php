b8a32edca29e2fef5b9cc88262ddaa19eb9d1c6f
Ardiputra614 <ardianto8306@gmail.com>
2022-12-19 10:43:46 +0700

    /web

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,6 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\KurirController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KeranjangController;
-<<<<<<< HEAD
-=======
-use App\Http\Controllers\SearchController;
-
-
->>>>>>> 45385c5 (toko)
 use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\TokoController;
